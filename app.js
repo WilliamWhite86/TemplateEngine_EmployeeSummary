2dda82437b9dfcf1a36d8b3386c7d89fcec16fb7
William White <william.white86@gmail.com>
2019-12-05 05:07:39 -0500

    changed intern and employee class so that intern and employee tests all pass

--- app.js
+++ app.js
@@ -3,51 +3,52 @@ const Employee = require("../TemplateEngine_EmployeeSummary/lib/Employee");
 const Intern = require("../TemplateEngine_EmployeeSummary/lib/Intern");
 
 function promptUser() {
-    return inquirer.prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is your name?"
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "What is your id?",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is your email?"
-      },
-      {
-       type: "list",
-       name: "role",
-       message: "What is your role?",
-       choices: ["Employee","Manager","Engineer","Intern"],
-      }
-    ]);
-  }
-  promptUser().then (function(value){
-  const e = new Employee(value.name,value.id,value.email,value.role)
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?"
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is your id?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email?"
+    },
+    {
+      type: "list",
+      name: "role",
+      message: "What is your role?",
+      choices: ["Employee", "Manager", "Engineer", "Intern"],
+    }
+  ]);
+}
+promptUser().then(function (value) {
+  let e = new Employee(value.name,value.id,value.email,value.role)
   console.log(e);
-  if(value.role === "Intern"){
-    promptIntern().then(function(value){
-      console.log(value)
-      const i = new Intern(e.name,e.id,e.email,e.role, value.school)
+  if (value.role === "Intern") {
+    promptIntern().then(function (value) {
+      //console.log(value)
+      let i = new Intern(e.name,e.id,e.email, value.school)
       console.log(i);
     })
-  }})
-  .catch(function(err) {
+  }
+})
+  .catch(function (err) {
     console.log(err)
   })
 
-  
-  function promptIntern() {
-      return inquirer.prompt([
-        {
-          type:"input",
-          name:"school",
-          message:"what is your school?"
-        } 
-      ])
-    };
+
+function promptIntern() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "school",
+      message: "what is your school?"
+    }
+  ])
+};
