a8a52d17e149eb1a9d816bedf47ef99b40e833b4
William White <william.white86@gmail.com>
2019-12-08 09:24:39 -0500

    added html templates and extended employee with manager constructor

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
-const inquirer = require("inquirer");
-const Employee = require("../TemplateEngine_EmployeeSummary/lib/Employee");
-const Intern = require("../TemplateEngine_EmployeeSummary/lib/Intern");
-const Manager = require("../TemplateEngine_EmployeeSummary/lib/Manager");
-const Engineer = require("../TemplateEngine_EmployeeSummary/lib/Engineer");
+const inquirer = require("inquirer")
+const Employee = require("../TemplateEngine_EmployeeSummary/lib/Employee")
+const Intern = require("../TemplateEngine_EmployeeSummary/lib/Intern")
+const Manager = require("../TemplateEngine_EmployeeSummary/lib/Manager")
+const Engineer = require("../TemplateEngine_EmployeeSummary/lib/Engineer")
+const employeeList = []
 
-promptManager().then(function (value) {
-  let m = new Manager(value.name, value.id, value.email, value.officeNumber)
-  console.log(m)
-  if (value.firstEmployee === true) {
-    createAnotherEmployee()
-  }
-})
+// promptManager().then(function (value) {
+//   let m = new Manager(value.name, value.id, value.email, value.officeNumber)
+//   console.log(m)
+//   if (value.firstEmployee === true) {
+//     createEmployee()
+//   }
+// })
 
-function createAnotherEmployee() {
+createEmployee()
+
+function createEmployee() {
   promptUser().then(function (value) {
     let e = new Employee(value.name, value.id, value.email)
     console.log(e)
-    if (value.role === "Intern") {
+    if (value.role === "Manager") {
+      promptManager().then(function (value){
+        let m = new Manager(e.name, e.id, e.email, value.officeNumber)
+        console.log(m)
+        employeeList.push(m)
+        console.log(employeeList)
+        if(value.anotherEmployee === true){
+          createEmployee()
+        }
+      })
+    }
+    else if (value.role === "Intern") {
       promptIntern().then(function (value) {
         let i = new Intern(e.name, e.id, e.email, value.school)
         console.log(i)
+        employeeList.push(i)
+        console.log(employeeList)
         if (value.anotherEmployee === true) {
-          createAnotherEmployee()
+          createEmployee()
         }
       })
     }
@@ -29,8 +45,10 @@ function createAnotherEmployee() {
       promptEngineer().then(function (value) {
         let en = new Engineer(e.name, e.id, e.email, value.gihub)
         console.log(en)
+        employeeList.push(en)
+        console.log(employeeList)
         if (value.anotherEmployee === true) {
-          createAnotherEmployee()
+          createEmployee()
         }
       })
     }
@@ -42,24 +60,24 @@ function promptUser() {
     {
       type: "input",
       name: "name",
-      message: "What is your employee's name?"
+      message: "What is the employee name?"
     },
     {
       type: "input",
       name: "id",
-      message: "What is your id?",
+      message: "What is the employee id?",
     },
     {
       type: "input",
       name: "email",
-      message: "What is your email?",
+      message: "What is the email?",
       validate: value => value.includes("@") && value.includes(".com") ? true : 'email invalid'
     },
     {
       type: "list",
       name: "role",
-      message: "What is your role?",
-      choices: ["Engineer", "Intern"]
+      message: "What role are we adding?",
+      choices: ["Manager","Engineer", "Intern"]
     }
   ])
 }
@@ -69,7 +87,7 @@ function promptIntern() {
     {
       type: "input",
       name: "school",
-      message: "what is your school?"
+      message: "what is their school?"
     },
     {
       type: "confirm",
@@ -81,22 +99,22 @@ function promptIntern() {
 
 function promptManager() {
   return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name?"
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is your id?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is your email?",
-      validate: value => value.includes("@") && value.includes(".com") ? true : 'email invalid'
-    },
+    // {
+    //   type: "input",
+    //   name: "name",
+    //   message: "What is your name?"
+    // },
+    // {
+    //   type: "input",
+    //   name: "id",
+    //   message: "What is your id?",
+    // },
+    // {
+    //   type: "input",
+    //   name: "email",
+    //   message: "What is your email?",
+    //   validate: value => value.includes("@") && value.includes(".com") ? true : 'email invalid'
+    // },
     {
       type: "input",
       name: "officeNumber",
@@ -104,8 +122,8 @@ function promptManager() {
     },
     {
       type: "confirm",
-      name: "firstEmployee",
-      message: "Would you like to add an employee?"
+      name: "anotherEmployee",
+      message: "Would you like to add another employee?"
     }
   ])
 }
@@ -115,7 +133,7 @@ function promptEngineer() {
     {
       type: "input",
       name: "gihub",
-      message: "what is your github?"
+      message: "what is their github?"
     },
     {
       type: "confirm",
