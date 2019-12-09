4bba1798e03f2c8f7499aca59d287137f33205e4
William White <william.white86@gmail.com>
2019-12-08 19:55:23 -0500

    added to html and style

--- templates/engineer.js
+++ templates/engineer.js
@@ -1,11 +1,11 @@
 function generateEngineerHTML(data) {
-    return `<div class="card" style="width: 25%">
+    return `<div class="card" style="width: 30%">
         <div class="card-header"><h1>${data.name}</h1></div>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item"><h2>Engineer</h2></li>
-                <li class="list-group-item">${data.id}</li>
-                <li class="list-group-item">${data.email}</li>
-                <li class="list-group-item">${data.github}</li>
+                <li class="list-group-item">ID: ${data.id}</li>
+                <li class="list-group-item">Email: ${data.email}</li>
+                <li class="list-group-item">GitHub: ${data.github}</li>
             </ul>
         </div>`
 }
--- templates/main.js
+++ templates/main.js
@@ -8,6 +8,14 @@ function generateMainHTML(data) {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+      .card {
+        float: left;
+      }
+      .jumbotron {
+        background-color: #53baff;
+      }
+    </style>
 </head>
 <body>
   <div class="jumbotron jumbotron-fluid">
